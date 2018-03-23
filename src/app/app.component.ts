0e7b649abbd970a2411ffed6bb6cf5382a860c53
ElkinDev <elkinmendoza00@gmail.com>
2018-03-23 17:28:33 -0500

    Using ngFor with Bootstrap List #4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 	title = 'New FrontPage';
-	name = '';
+	name='Lista 1'
+	Lists=[
+	{id:1,name:'List one',description:'This is description 1',cost:5000},
+	{id:2,name:'List Two',description:'This is description 2',cost:5000},
+	{id:3,name:'List Three',description:'This is description 3',cost:5000},
+	{id:4,name:'List1 For',description:'This is description 4',cost:5000},
+	];
+	
 }
